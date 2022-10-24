b8b4d084d224dcf970b4f5b4b2711fc460ecf357
SuperCater <supercater500@gmail.com>
2022-10-23 20:59:58 -0400

    Cleaned up utility.js
    Removed all the unneeded functions.

--- SRC/modules/utils.js
+++ SRC/modules/utils.js
@@ -4,30 +4,7 @@ const { advancedTypes } = require("../options.js");
 const fs = require("fs");
 const { parseArgs } = require("util");
 
-// Convert strings to numbers
-// TODO: Convert precents to decimals
 
-const stringToNumber = (str) => {
-  for (let i = 0; i < str.length; i++) {
-    if (typeof str[i] === "string" && str[i].includes("%") && !str[i] === "%") {
-      str[i] = str[i].replace("%", "");
-      str[i] = str[i] / 100;
-    }
-    if (
-      str[i] === "+" ||
-      str[i] === "-" ||
-      str[i] === "*" ||
-      str[i] === "/" ||
-      str[i] === "%" ||
-      str[i] === "**"
-    ) {
-      str[i] === str[i];
-    } else {
-      str[i] = parseFloat(str[i]);
-    }
-  }
-  return str;
-};
 const addCommas = (num) => {
   return num.toLocaleString();
 };
@@ -54,71 +31,7 @@ const errorChecker = (values) => {
   }
 };
 
-// ! Being fixed
-const stringToArray = (str) => {
-  let splitStr = []
-  for (let i = 0; i < str.length; i++) {
-    if (str[i] === "+" || str[i] === "-" || str[i] === "*" || str[i] === "/" || str[i] === "%" || str[i] === "*") {
-      if (str[i] === "*" && str[i + 1] === "*") {
-        splitStr.push(str[i] + str[i + 1]);
-        i++;
-      } else {
-        splitStr.push(str[i])
-      }
-      } else {
-      if (str[i] !== "+" || str[i] !== "-" || str[i] !== "*" || str[i] !== "/" || str[i] !== "%" || str[i] !== "**") {
-        if (str[i + 1] === "+" || str[i + 1] === "-" || str[i + 1] === "*" || str[i + 1] === "/" || str[i + 1] === "%" || str[i+1] === undefined) {
-          splitStr.push(str[i])
-        } else {
-          splitStr.push(str[i] + str[i + 1]);
-          i++;
-        }
-      } 
-    }
-  }
-  return splitStr;
-}
-
-// ! IN DEVELOPMENT
-const stringToArray2 = (str) => {
-  stringSep = [];
-  for (let i = 0; i < str.length; i++) {
-    if (str[i] === 5) {}
-}
-}
-
-// convert a string to an array
-
-const checkIfType = (...values) => {
-  if (typeof values[0] === "string") {
-    values[0] === values[0].toLowerCase();
-    if (
-      values[0] === "tax" ||
-      values[0] === "square" ||
-      values[0] === "circumference" ||
-      values[0] === "radius" ||
-      values[0] === "circlearea" ||
-      values[0] === "squarearea" ||
-      values[0] === "delete"
-    ) {
-      return true;
-    }
-  }
-  return false;
-};
-
-// lowercase
-// LEGACY: A lot of these utility funtions are no longer used and probably won't work with the code. I'm keeping them here for now just in case I need them later.
-const lowerCase = (str) => {
-  return str.toLowerCase();
-};
-
-const getInfo = () => {
-  return {
-    version: version,
-  };
-};
-
+// * Needs to be updated
 const random = (max, values) => {
   for (let i = 0; i < values; i++) {
     options.values[i] = Math.floor(Math.random() * max);
@@ -182,45 +95,10 @@ const savingDataNormal = (data) => {
   });
 };
 
-const symbolConversion = (symbol) => {
-  switch (symbol) {
-    case "+":
-      return "add";
-    case "-":
-      return "subtract";
-    case "*":
-      return "multiply";
-    case "/":
-      return "divide";
-    case "%":
-      return "remainder";
-    case "**":
-      return "exponent";
-    default:
-      return symbol;
-      break;
-  }
-};
-
-const checkIndex = (values, ...check) => {
-  let index;
-  index = values.findIndex((value) => {
-    value === check[0] || value === check[1];
-  });
-  return index;
-};
-
 // exports the functions to be used in other files
 module.exports = {
-  stringToNumber,
   addCommas,
-  lowerCase,
-  getInfo,
+  errorChecker,
   random,
   savingDataNormal,
-  symbolConversion,
-  checkIndex,
-  errorChecker,
-  stringToArray,
-  checkIfType,
 };
