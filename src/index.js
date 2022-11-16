f700790657904d952537a001480ea58e6c4ec2d7
SuperCater <supercater500@gmail.com>
2022-11-16 01:32:14 -0500

    added a cool message

--- src/index.js
+++ src/index.js
@@ -46,5 +46,7 @@ app.on('activate', () => {
   }
 });
 
+console.log("Hey if you're seeing this you're awesome!");
+
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
