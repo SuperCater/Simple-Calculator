3b7bf6f4e0883c845ee3dcb6337118e474c1be58
SuperCater <supercater500@gmail.com>
2022-11-17 05:30:53 -0500

    Added basic calculate functionality
    added the basic calculating functionality, just need to add all the buttons, style them, and pray to god it doesn't break.

---
+++ src/modules/calculate.js
@@ -0,0 +1,51 @@
+const calculate = (...cValues) => {
+  // Initalize variables for later
+  let result;
+  let index;
+  // Loop through the array to do all the operations.
+  let i = 0;
+  while (i < 500) {
+    i++
+    index = cValues.findIndex((value) => value === "**"); // Find exponents first
+    if (index === -1) {
+      index = cValues.findIndex(
+        (value) => value === "*" || value === "/" || value === "%" // Finds multiplication, division, and modulus left to right
+      );
+      if (index === -1) {
+        index = cValues.findIndex((value) => value === "+" || value === "-"); // finds addition and subtraction left to right
+      }
+    }
+    switch (cValues[index]) {
+      case "*":
+        result = cValues[index - 1] * cValues[index + 1];
+        break;
+      case "/":
+        result = cValues[index - 1] / cValues[index + 1];
+        break;
+      case "%":
+        result = cValues[index - 1] % cValues[index + 1];
+        break;
+      case "+":
+        result = cValues[index - 1] + cValues[index + 1];
+        break;
+      case "-":
+        result = cValues[index - 1] - cValues[index + 1];
+        break;
+    case "**":
+        result = cValues[index - 1] ** cValues[index + 1];
+        break;
+    }
+
+    cValues.splice(index - 1, 1); // Removes first value
+    cValues.splice(index, 1); // Removes second value
+    cValues[index - 1] = result;
+    if (cValues.length === 1) {
+      break;
+    }
+  }
+  console.log(cValues);
+  return parseFloat(cValues);
+};
+
+// Export the calculate function
+export { calculate };
--- src/render.js
+++ src/render.js
@@ -1,10 +1,13 @@
 // Buttons
-
+// Import the calculate module with ES6 syntax
+import { calculate } from "./modules/calculate.js";
 let equation = [];
 
 const dEquation = document.getElementById('equation');
 const addButton = document.getElementById('add');
 const subtractButton = document.getElementById('subtract');
+const submit = document.getElementById('submit');
+const five = document.getElementById('five');
 
 addButton.onclick = function() {
     equation.push('+');
@@ -15,6 +18,22 @@ addButton.onclick = function() {
 
 subtractButton.onclick = function() {
     equation.push('-');
+    console.log(equation);
+    // Change the text to the equation variable
+    dEquation.innerHTML = equation;
+}
+
+submit.onclick = function() {
+    // Calculate the equation
+    console.log(equation);
+    equation = calculate(...equation);
+    console.log(equation);
+    dEquation.innerHTML = equation;
+    equation = [];
+}
+
+five.onclick = function() {
+    equation.push(5);
     console.log(equation);
     // Change the text to the equation variable
     dEquation.innerHTML = equation;
