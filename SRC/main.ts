abf49bc5ec9c9eb0148faffd8dd5902e802a597d
SuperCater <supercater500@gmail.com>
2024-01-15 19:14:13 -0500

    new calculate module

---
+++ SRC/main.ts
@@ -0,0 +1,8 @@
+// TODO: Accept user input, for now just use this for testing
+
+import calculate from "./modules/calculate.js";
+
+const expression = "(55+5)*(2+3)";
+
+
+console.log(calculate(expression));
